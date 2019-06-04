2a9fae0e2856a53c1ace9aac9c171ca3f2952dd3
Skyler Jane Clark <t-skclar@microsoft.com>
2019-06-03 17:03:32 -0700

    restructures basically everything lol

--- projects/cli/mtool/mtool/helpers/file_io.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def load_json(filename):
-    with open(filename, encoding="utf8") as json_file:
-        return json.load(json_file)
-
-def save_json(filename, contents):
-    with open(filename, 'w', encoding="utf8") as outfile:
-        json.dump(contents, outfile, indent=4)
