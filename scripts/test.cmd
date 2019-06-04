1c072edb611fb71713bcbb1c374215a534a74bfe
Skyler Jane Clark <t-skclar@microsoft.com>
2019-06-04 10:44:29 -0700

    renames files and creates runner.py

--- scripts/test.cmd
+++ scripts/test.cmd
@@ -1,25 +1,20 @@
 @ECHO OFF
-set "MTOOL_DIR=../mtool"
-set "ENVIRONMENT_DIR=/environment"
-set "CLI_DIR=/cli"
-set "LIBRARY_DIR=/library"
-set "NOTEBOOK_DIR=/notebook"
-set "PANSOP_DIR=/pansop"
-set "SCENE_DIR=/scene"
-set "UTIL_DIR=/util"
+CALL python ..\runner.py "diagnose" "me"
+CALL python ..\runner.py "run_notebook" "notebook"
+CALL python ..\runner.py "open_notebook" "this notebook"
+CALL python ..\runner.py "search_notebook" "SOP"
+CALL python ..\runner.py "list_notebook" 
+CALL python ..\runner.py "history" 
+CALL python ..\runner.py "next_notebook" 
+CALL python ..\runner.py "new_scene" 
+CALL python ..\runner.py "end_scene" 
+CALL python ..\runner.py "change_scene" 
+CALL python ..\runner.py "resume_scene" 
+CALL python ..\runner.py "delete_scene" 
+CALL python ..\runner.py "list_scene" 
+CALL python ..\runner.py "add_library" 
+CALL python ..\runner.py "set_env" 
+CALL python ..\runner.py "delete_env" 
 
-set "ENVIRONMENT=%MTOOL_DIR%%ENVIRONMENT_DIR%"
-set "CLI=%MTOOL_DIR%%CLI_DIR%"
-set "LIBRARY=%MTOOL_DIR%%LIBRARY_DIR%"
-set "NOTEBOOK=%MTOOL_DIR%%NOTEBOOK_DIR%"
-set "PANSOP=%MTOOL_DIR%%PANSOP_DIR%"
-set "SCENE=%MTOOL_DIR%%SCENE_DIR%"
-set "UTIL=%MTOOL_DIR%%UTIL_DIR%"
 
-echo %ENVIRONMENT%
-echo %CLI%
-echo %LIBRARY%
-echo %NOTEBOOK%
-echo %PANSOP%
-echo %SCENE%
-echo %UTIL%
+
