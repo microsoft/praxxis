687355590b82f0eb6ee3eaf17848ec3fbc7257eb
Skyler Jane Clark <t-skclar@microsoft.com>
2019-06-11 16:09:04 -0700

    allows current_scene to run as its own process

--- src/mtool/scene/current_scene.py
+++ src/mtool/scene/current_scene.py
@@ -7,11 +7,24 @@ Dependencies within mtool: mtool/mtool.py
 import os
 import sys
 
-from src.mtool.cli import mtool
-
 def current_scene(args):
-    """Sets up environment for current scene"""
-    from src.mtool.cli import scene 
+    """Sets up environment for current scene""" 
+    from src.mtool.scene import scene
+    from src.mtool.util import sqlite_util
+
+    root = os.path.join(os.getenv('APPDATA'),"mtool","scene")
+    default_scene_name = 'scene'
+
+    current_scene = os.path.join(root, "current_scene.db")
+
+    if not os.path.exists(root):
+        os.mkdir(root)
+        
+    if not os.path.exists(current_scene):
+        scene.new_scene(default_scene_name, default_scene_name)
+        sqlite_util.init_current_scene(os.path.join(root, "current_scene.db"), default_scene_name)
+        print("Created current_scene")
+
+    print(sqlite_util.get_current_scene(current_scene)) 
+    return("nya?")   
     
-    m = mtool.Mtool(args)
-    #m.set_environment_overrides_for_scene()
