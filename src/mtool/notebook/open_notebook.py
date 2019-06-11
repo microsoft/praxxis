6bcb6e364b93fb0e987f050f2a27e1051ec426ae
Amanda Bertsch <t-ambert@microsoft.com>
2019-06-11 15:23:29 -0700

    run_notebook works, though it lacks bells and whistles

--- src/mtool/notebook/open_notebook.py
+++ src/mtool/notebook/open_notebook.py
@@ -10,6 +10,7 @@ import shutil
 import subprocess
 
 import pypandoc
+import webbrowser
 
 from src.mtool.notebook.notebook import Notebook
 from src.mtool.scene.scene import Scene
@@ -18,7 +19,7 @@ def open_notebook(args):
     """Open a notebook"""
     # notebook = the number of the notebook
     notebook_filename = Scene.ordinal_to_list_item(args.notebook)
-    load_notebook(notebook_filename)
+    display_in_editor(notebook_filename)
     #load_notebook(m.args.ordinal_to_list_item(args)[0])
 
 def azure_data_studio_binary_location():
@@ -26,14 +27,14 @@ def azure_data_studio_binary_location():
     """Returns location of ADS binary"""
     return os.path.join(os.getenv('LOCALAPPDATA'), 'Programs', 'Azure Data Studio', 'azuredatastudio')
 
-
-def convert_to_html(filename, html_outputfile):
+def display_as_html(filename, html_outputfile):
     pypandoc.convert_file(filename, 'html', outputfile=html_outputfile)
+    webbrowser.open(html_outputfile)
 
 def display_to_console(filename):
     print(pypandoc.convert_file(filename, 'asciidoc'))
 
-def load_notebook(filename):
+def display_in_editor(filename):
     """Opens a notebook in Azure Data Studio
 
     Keyword arguments:
--- src/mtool/notebook/run_notebook.py
+++ src/mtool/notebook/run_notebook.py
@@ -2,100 +2,102 @@
 This file runs a notebook. Results are either printed to the console or 
 opened as an html output in the web browser, depending on user input.
 
-Dependencies within mtool: mtool/mtool.py
+Dependencies within mtool: log.py, notebook.py, scene.py, open_notebook.py,
+    telemetry.py
 """
 
 import os
 import sys
 import webbrowser
-from src.mtool.cli import mtool
-from src.mtool.notebook import notebook
-import time
+from datetime import datetime
 
-m = None
+from src.mtool.util.log import Log
+from src.mtool.notebook.notebook import Notebook
+from src.mtool.scene.scene import Scene
+from src.mtool.notebook import open_notebook 
+from src.mtool.util import telemetry
+
+import papermill
 
 def run_notebook(args):
     """Runs one notebook specified"""
-    print(args)
-    global m
-    m = mtool.Mtool(args)
-    m.set_environment_overrides_for_scene()
+    run(Scene.ordinal_to_list_item(args.notebook))
 
-    m.for_each_notebook_specified_on_command_line(run_notebook)
-
-
-
-def execute(self):
-    if not(self._hasParameters):
+def execute(notebook):
+    """Handles papermill execution for notebook"""
+    local_copy = get_outputname(notebook)
+    if (notebook._hasParameters):
+        injects = pull_params(notebook)
+        papermill.execute_notebook(notebook.path, local_copy, injects)
+    else:
         print("Warning: no tagged cell located. No parameters will be " +
             "injected for this notebook.")
+        papermill.execute_notebook(notebook.getpath(), local_copy)
+    
     #need local output -- temp? or just send it directly to HDFS
     # need to pull params from toml and send to papermill as dict
-    local_copy = os.path.join(os.path.expandvars(self._output_loc), "20190607" + str(time.time()) + self.name.split(".")[0] + "-"  + ".ipynb")
-    papermill.execute_notebook(self._path, local_copy)
+    
     return local_copy
 
-def pull_params(self):
+def pull_params(environmentVars):
+    """Returns a dictionary of all overridden parameters for notebook"""
+    return {}
+
+def get_outputname(notebook):
+    """Returns name of output file for notebook"""
+    appdata = os.path.expandvars("%APPDATA%")
+    output_folder = os.path.join(appdata, "mtool", "output")
+
+    timestamp = datetime.today().strftime("%Y%m%d-%H%M%S")
+    filename = f"{timestamp}-{notebook.library_name}-{notebook.name}"
     
-    return
-
-
+    outputname = os.path.join(output_folder, filename)
+    return outputname
 
 def run(filename):
-    """Run a notebook.
+    """Run a notebook, given a filename.
 
     Keyword arguments:
     filename -- the name of a notebook to run
-    Keyword exceptions:
-    if notebook fails to execute correctly (raises error in running)
     """
     
-    nb = notebook.Notebook(filename)
-    log = m.log
-    spinner = m.spinner
+    notebook = Notebook(filename)
+    log = Log()
 
-    local_copy = nb.name 
-    html_outputfile = nb.name.split(".")[0] + "-" + str(time.time()) + ".ipynb"
 
     log.header("Running")
-    print("testing")
-    log.indent_no_new_line("{0}... ".format(nb.name))
+    log.indent_no_new_line("{0}... ".format(notebook.name))
 
-    spinner.start()
-    nb.execute()
+    local_copy = execute(notebook)
 
-    # BUG: There appears to be a bug in jupyter nbconvert where you can't
-    # save the results of a notebook if an error was raised in one of the cells!
-    # 
-    # This is frustrating, because it means we can't get any telemetry on notebooks
-    # that partially completed!  We need to find a fix for this!
-    #
-    # Of course, we could pass in 'allow_errors' = True above, but we don't
-    # want to do this (without the user expressly allowing us to) because we
-    # might do something really bad, by running cells in a notebook that presume
-    # the cells above have run successfully!
-    #
-    if not os.path.isfile(local_copy):
-        raise Exception("\n\nNotebook execution raised an error, please 'm open' this notebook and execute the cells to view the error.")
-
-    if m.show_notebook_in_web_browser:
-        nb.convert_to_html(local_copy, html_outputfile)
-
-    spinner.stop()
-
-    if not m.show_notebook_in_web_browser:
-        log.info("")
-        log.header("Notebook output")
-        nb.display_to_console()
-    else:
+    # TODO: html flag
+    if False:
+        html_outputfile = f"{local_copy.split('.')[0]}.html"
         log.complete()
 
         log.header("View HTML output from notebook runs here")
-        log.indent("start", m.working_dir)
+        log.indent("%APPDATA%/mtool/output")
         log.header("Launching result file in web browser")
         log.indent(html_outputfile)
+        open_notebook.display_as_html(local_copy, html_outputfile)
+    else:
+        log.info("")
+        log.header("Notebook output")
+        open_notebook.display_to_console(local_copy)
 
-        webbrowser.open(html_outputfile)
+    send_telemetry(local_copy)
 
-    m.send_telemetry(local_copy)
+def send_telemetry(local_copy):
+    """Sends telemetry to HDFS"""
+    basedir = os.path.expandvars("%APPDATA%/mtool")
+    with open(os.path.join(basedir, "id.json")) as infile:
+        id = infile.read()
+    infile.close()
+    with open(os.path.join(basedir, "scene", "current_scene.json")) as infile:
+        curr_scene = infile.read()
+    infile.close()
+    with open(os.path.join(basedir, "scene", curr_scene, "id.json")) as infile:
+        scene_id = infile.read()
+    infile.close()
 
+    telemetry.send(id, scene_id, local_copy)
