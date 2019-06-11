797d08c4496ebe0745cb13c9fd87f9b92f68b349
Amanda Bertsch <t-ambert@microsoft.com>
2019-06-11 10:52:41 -0700

    fixes list_notebook

--- src/mtool/notebook/list_notebook.py
+++ src/mtool/notebook/list_notebook.py
@@ -17,6 +17,46 @@ counter = 0
 items = []
 display_format_string = "\t{0}.\t{1} ({2})"
 
+def display(nb):
+    """Displays a notebook name as part of a list.
+    
+    Keyword arguments:
+    filename -- notebook name to display
+    """
+    global counter
+    global items
+    global display_format_string
+    counter += 1
+
+    l.info(display_format_string.format(str(counter), nb.name, nb.library_name))
+    items.append([counter, nb.name, nb.library_name])
+
+def display_each_notebook(root = "", notebooks = []):
+    """Calls fn on every notebook in directory
+
+    Default: calls fn on every notebook in library folder
+    """
+    # TODO: %APPDATA% is windows specific and gross
+    # note: the weird default variable thing is because you can't call
+    #   a function in a arguments list :/
+    if root == "":
+        root = os.path.join(os.getenv('APPDATA'),"mtool","library")
+    os.chdir(root)
+    currDir = os.listdir()
+    subDirs = []
+    for item in currDir:
+        if os.path.isfile(item) and item.endswith(".ipynb"):
+            path = os.getcwd()
+            nb = notebook.Notebook(os.path.join(path, item))
+            notebooks.append(item)
+            display(nb)
+        elif os.path.isdir(item):
+            subDirs.append(item)
+    while subDirs != []:
+        thisSubDir = subDirs.pop()
+        subDirPath = os.path.join(root, thisSubDir)
+        display_each_notebook(subDirPath, notebooks)
+        
 def list_notebook(args):
     """Display each notebook found on machine"""
     global l
@@ -29,21 +69,6 @@ def list_notebook(args):
         for item in m.get_list:
             l.info(display_format_string.format(str(item[0]), item[1], item[2]))
     else:
-        notebook.Notebook.for_each_notebook(display)
+        display_each_notebook()
         m.write_list(items)
-
-
-def display(notebook):
-    """Displays a notebook name as part of a list.
-    
-    Keyword arguments:
-    filename -- notebook name to display
-    """
-    global counter
-    global items
-    global display_format_string
-    counter += 1
-
-    l.info(display_format_string.format(str(counter), notebook.name, notebook.library_name))
-    items.append([counter, notebook.name, notebook.library_name])
 
