2f0ca5939845ca542c7340902d9abf2986eb065e
Skyler Jane Clark <t-skclar@microsoft.com>
2019-06-12 12:12:48 -0700

    sets up ending scene, list scene and fixes current scene

--- src/mtool/scene/current_scene.py
+++ src/mtool/scene/current_scene.py
@@ -19,11 +19,12 @@ def current_scene(root):
         os.mkdir(root)
         
     if not os.path.exists(current_scene):
-        new_scene.new_scene(default_scene_name, root)
         sqlite_util.init_current_scene(os.path.join(root, "current_scene.db"), default_scene_name)
+        print("Created current_scene")
+        new_scene.new_scene(default_scene_name, root)
+
 
 ##TODO: REMOVE PRINTS, ENVIRONMENT LOADING ??
-        print("Created current_scene")
 
     print(f'Current Scene: {sqlite_util.get_current_scene(current_scene)}') 
     return current_scene
