50d345e700e4a2f1853d550210336706d743f7eb
Skyler Jane Clark <t-skclar@microsoft.com>
2019-06-12 13:07:39 -0700

    sets up environment SQL

--- src/mtool/environment/set_env.py
+++ src/mtool/environment/set_env.py
@@ -6,11 +6,20 @@ Dependencies within mtool: mtool/mtool.py
 """
 
 import os
-import sys
 
-from src.mtool.cli import mtool
+def set_env(args, root):
+    from src.mtool.util import sqlite_util
+    if hasattr(args, "name"):
+        name = args.name
+    else:
+        name = args
 
-def set_env(args):
-    """Call mtool method to set environment variables"""
-    m = mtool.Mtool(args)
-    m.set_env()
+    current_scene = os.path.join(root, "current_scene.db")
+    scene = sqlite_util.get_current_scene(current_scene)
+
+    directory = os.path.join(root, scene)
+    db_file = os.path.join(directory, f"{scene}.db")
+
+    sqlite_util.set_env(db_file, args.name, args.value)
+
+
