62f9b5cf7fcd1b3d18c9fe7399461f754932d984
Skyler Jane Clark <t-skclar@microsoft.com>
2019-06-13 16:09:10 -0700

    adds ordinal commands to scene

--- src/mtool/scene/list_scene.py
+++ src/mtool/scene/list_scene.py
@@ -9,10 +9,16 @@ import os
 def list_scene(root, history_db):
     from src.mtool.util import sqlite_util
     from src.mtool.cli import display
-    
+
     ended_scenes = sqlite_util.get_ended_scenes(history_db)
     active_scenes = sqlite_util.get_active_scenes(history_db)
     current_scene = sqlite_util.get_current_scene(history_db)
+
+    sqlite_util.dump_scene_list(history_db)
+
+    sqlite_util.write_scene_list(history_db, ended_scenes)
+    sqlite_util.write_scene_list(history_db, active_scenes)
+    sqlite_util.write_scene_list(history_db, [(current_scene,)])
 
     display.display_list_scene(ended_scenes, active_scenes, current_scene)
 
