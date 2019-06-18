5af4b7ffef1ba3f273abeebd41ebf00201c95b88
Skyler Jane Clark <t-skclar@microsoft.com>
2019-06-18 09:57:46 -0700

    stubs out notebook tests

--- src/mtool/notebook/list_notebook.py
+++ src/mtool/notebook/list_notebook.py
@@ -2,7 +2,7 @@
 This file lists all of the notebooks loaded into the library db file
 """
         
-def list_notebook(scene_root, library_db, history_db, current_scene_db, start, stop):
+def list_notebook(scene_root, library_db, current_scene_db, start, stop):
     """ gets the notebooks from the sqlite db and displays them through its display function"""
     from src.mtool.util import sqlite_util
     from src.mtool.cli import display
