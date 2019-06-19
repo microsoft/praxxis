8d518a78b65ca042f1250d084d31f5638c406e11
Amanda Bertsch <t-ambert@microsoft.com>
2019-06-18 17:08:28 -0700

    stopgap fix for search with no notebooks loaded

--- src/mtool/notebook/search_notebook.py
+++ src/mtool/notebook/search_notebook.py
@@ -1,10 +1,14 @@
 """
 Searches notebooks for search term using a sql query, and calls the display function
 """
+import os
 
 def search_notebook(args, library_db, start, end):
     """ searches and displays loaded notebooks"""
     from src.mtool.util import sqlite_util
     from src.mtool.cli import display
+    if not os.path.exists(library_db):
+        from src.mtool.library.library import init_library
+        init_library(os.path.dirname(library_db), library_db)
     search_term = args.term
     display.display_search(search_term, sqlite_util.search_notebooks(library_db, search_term, start, end))
