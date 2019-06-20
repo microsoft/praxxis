e58dfea79e5301257b7bdf7b446aac2b73264912
Amanda Bertsch <t-ambert@microsoft.com>
2019-06-19 17:39:11 -0700

    allow ds and es to refer to current scene if no args

--- src/mtool/scene/delete_scene.py
+++ src/mtool/scene/delete_scene.py
@@ -12,8 +12,12 @@ def delete_scene(args, scene_root, history_db):
     from src.mtool.cli import display
     from src.mtool.scene import scene
 
+
     if hasattr(args, "name"):
-        name = args.name
+        if(args.name == None):
+            name = sqlite_util.get_current_scene(history_db)
+        else:
+            name = args.name
     else:
         name = args
 
--- src/mtool/scene/end_scene.py
+++ src/mtool/scene/end_scene.py
@@ -7,9 +7,12 @@ def end_scene(args, scene_root, history_db, current_scene_db):
     from src.mtool.util import sqlite_util
     from src.mtool.cli import display
     from src.mtool.scene import scene
-    
+
     if hasattr(args, "name"):
-        name = args.name
+        if(args.name == None):
+            name = sqlite_util.get_current_scene(history_db)
+        else:
+            name = args.name
     else:
         name = args
 
