4a9306a6d458f8be39756f9176a9ef298157c8b7
Skyler Jane Clark <t-skclar@microsoft.com>
2019-06-20 08:46:49 -0700

    allows es to have 0 args

--- src/mtool/scene/end_scene.py
+++ src/mtool/scene/end_scene.py
@@ -17,6 +17,9 @@ def end_scene(args, scene_root, history_db, current_scene_db):
     if tmp_name != None:
         name = tmp_name
 
+    if args.name == None and tmp_name == None:
+        name = sqlite_util.get_current_scene(history_db)
+
     allow_end_scene = sqlite_util.mark_ended_scene(history_db, name)
 
     if allow_end_scene == -1:
