1fd6aa9f337808d12631551ee68f8ac90fbdefe9
Skyler Jane Clark <t-skclar@microsoft.com>
2019-06-27 12:55:36 -0700

    fixes some display issues

--- src/mtool/cli/display.py
+++ src/mtool/cli/display.py
@@ -161,13 +161,14 @@ def display_view_env(envs, set_envs):
     else:
         set_envs = dict(set_envs)
         envs = dict(envs)
+        counter = 0
         for name in envs:
-            print(f"{name} = ", end="")
+            counter += 1
+            print(f"\t{counter}.\t{name} = ", end="")
             if name in set_envs:
                 print(f"{Fore.BLUE}{set_envs[name]}{Style.RESET_ALL}" )
             else:
                 print(name)
-
 
 
 
