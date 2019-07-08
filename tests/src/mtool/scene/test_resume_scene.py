3cc00f125e6571c13820a9d9ca72f97521f74961
Skyler Jane Clark <t-skclar@microsoft.com>
2019-07-08 09:14:34 -0700

    adds tests for resuming scenes

--- tests/src/mtool/scene/test_resume_scene.py
+++ tests/src/mtool/scene/test_resume_scene.py
@@ -1,12 +1,9 @@
-def test_resume_scene(setup, scene_root, history_db):
+def test_resume_scene(setup, create_ended_scene, scene_root, history_db, current_scene_db):
     from src.mtool.scene import resume_scene
-    from tests.src.mtool.util import dummy_object
     from src.mtool.util.sqlite import sqlite_scene
-
-    # notebook1 = dummy_name_object.make_dummy_scene("generated_ended_scene")
-    # resume_scene.resume_scene(notebook1, scene_root, history_db)
-    # ended_scenes = sqlite_scene.get_ended_scenes(history_db)
-    # print(ended_scenes)
-
-    # assert "generated_ended_scene" in sqlite_scene.get_ended_scenes(history_db)
-
+    
+    ended_scenes = sqlite_scene.get_ended_scenes(history_db)
+    assert len(ended_scenes) == 1
+    resume_scene.resume_scene("generated_ended_scene", scene_root, history_db)
+    ended_scenes = sqlite_scene.get_ended_scenes(history_db)
+    assert len(ended_scenes) == 0
