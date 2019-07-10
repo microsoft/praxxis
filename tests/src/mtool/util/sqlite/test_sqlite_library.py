8963cac583770d6fcecb1955bdc0cc44bffcab1a
Skyler Jane Clark <t-skclar@microsoft.com>
2019-07-10 13:54:52 -0700

    fixes test

--- tests/src/mtool/util/sqlite/test_sqlite_library.py
+++ tests/src/mtool/util/sqlite/test_sqlite_library.py
@@ -13,6 +13,8 @@ def test_init_library_db(setup, library_db):
     check_library_metadata_table = f"SELECT count(*) FROM sqlite_master WHERE type='table' AND name='LibraryMetadata';"
     check_notebooks_table = f"SELECT count(*) FROM sqlite_master WHERE type='table' AND name='Notebooks';"
     check_environment_table = f"SELECT count(*) FROM sqlite_master WHERE type='table' AND name='Environment';"
+    check_notebook_environment_table = f"SELECT count(*) FROM sqlite_master WHERE type='table' AND name='NotebookEnvironment';"
+
 
     cur.execute(check_library_metadata_table)
     library_metadata = bool(cur.fetchone()[0])
@@ -23,13 +25,18 @@ def test_init_library_db(setup, library_db):
     cur.execute(check_environment_table)
     environment = bool(cur.fetchone()[0])
 
+    cur.execute(check_notebook_environment_table)
+    notebook_environment = bool(cur.fetchone()[0])
+
     assert library_metadata
     assert notebooks
     assert environment
+    assert notebook_environment
 
     check_library_metadata_columns = f"SELECT * FROM'LibraryMetadata';"
     check_notebooks_columns = f"SELECT * FROM 'Notebooks';"
     check_environment_columns = f"SELECT * FROM 'Environment';"
+    check_notebook_environment_columns = f"SELECT * FROM 'NotebookEnvironment'"
 
     cur.execute(check_library_metadata_columns)
     library_metadata_columns = [description[0] for description in cur.description]
@@ -40,6 +47,10 @@ def test_init_library_db(setup, library_db):
     cur.execute(check_environment_columns)
     environment_columns = [description[0] for description in cur.description]
 
+    cur.execute(check_notebook_environment_columns)
+    notebook_environment_columns = [description[0] for description in cur.description]
+
     assert library_metadata_columns == ['Root', 'Readme', 'Name']
     assert notebook_columns == ['Root', 'Name', 'LibraryName']
-    assert environment_columns == ['Name', 'Value', 'NotebookName']
+    assert environment_columns == ['Name', 'Value']
+    assert notebook_environment_columns == ['EnvironmentName', 'NotebookName']
