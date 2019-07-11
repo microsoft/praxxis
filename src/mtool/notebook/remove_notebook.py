00fb38b99a2d555523d260fd03b691baa6603376
Skyler Jane Clark <t-skclar@microsoft.com>
2019-07-11 11:41:12 -0700

    adds error when removing nonexistent notebook

--- src/mtool/notebook/remove_notebook.py
+++ src/mtool/notebook/remove_notebook.py
@@ -1,5 +1,6 @@
 def remove_notebook(args, library_db, current_scene_db):
     from src.mtool.util.sqlite import sqlite_library
+    from src.mtool.util.sqlite import sqlite_notebook
     from src.mtool.notebook import notebook
     from src.mtool.util import error
     from src.mtool.notebook import list_notebook
@@ -15,5 +16,10 @@ def remove_notebook(args, library_db, current_scene_db):
     if tmp_name != None:
         name = tmp_name
     
+    try:
+        sqlite_notebook.check_notebook_exists(library_db, name)
+    except error.NotebookNotFoundError as e:
+        raise e
+
     sqlite_library.remove_notebook(library_db, name)
     display_notebook.display_remove_success(name)
