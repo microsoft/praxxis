1478ea5de9f6c45f3e3d8c77039e83fb345f6059
Skyler Jane Clark <t-skclar@microsoft.com>
2019-07-12 12:24:41 -0700

    adds search notebooks test

--- tests/src/mtool/util/test_cli.py
+++ tests/src/mtool/util/test_cli.py
@@ -20,6 +20,7 @@ def test_command(setup,
     from src.mtool.notebook import run_notebook
     from src.mtool.environment import list_env
     from src.mtool.notebook import open_notebook
+    from src.mtool.notebook import search_notebook
 
     list_notebook.list_notebook(library_db, current_scene_db, start, stop)
 
@@ -36,3 +37,9 @@ def test_command(setup,
     dummy_input = dummy_object.make_dummy_input("open_notebook")
     result = cli.command(dummy_input, init_root, library_root, library_db, outfile_root, scene_root, history_db, telemetry_db, default_scene_name, True)
     assert result.__class__ == open_notebook.open_notebook.__class__
+
+    dummy_input = dummy_object.make_dummy_input("search_notebooks")
+    result = cli.command(dummy_input, init_root, library_root, library_db, outfile_root, scene_root, history_db, telemetry_db, default_scene_name, True)
+    assert result.__class__ == search_notebook.search_notebook.__class__
+
+    
