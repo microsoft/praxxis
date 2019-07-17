eb292489a3da7a909f6d1186d43162a2d2720646
Skyler Jane Clark <t-skclar@microsoft.com>
2019-07-17 10:10:59 -0700

    fixes a bunch of tests

--- tests/src/mtool/util/entrypoints/test_entry_scene.py
+++ tests/src/mtool/util/entrypoints/test_entry_scene.py
@@ -16,41 +16,70 @@ def test_init_scene(setup, scene_root, history_db, default_scene_name):
     assert os.path.exists(scene_root)
 
 def test_delete_scene(setup, create_one_scene, scene_root, history_db):
-    delete_scene = entry_scene.delete_scene("generated_one_scene", scene_root, history_db)
+    from src.mtool.scene import list_scene
+    entry_scene.delete_scene("generated_one_scene", scene_root, history_db)
 
-    assert delete_scene == "generated_one_scene"
+    assert len(list_scene.list_scene(scene_root, history_db)) == 1
 
     
 def test_resume_scene(setup, create_ended_scene, scene_root, history_db):
-    resume_scene = entry_scene.resume_scene("generated_ended_scene", scene_root, history_db)
+    from src.mtool.util.sqlite import sqlite_scene
+    from src.mtool.util import error
 
-    assert resume_scene == "generated_ended_scene"
+    entry_scene.resume_scene("generated_ended_scene", scene_root, history_db)
+    try:
+        sqlite_scene.check_scene_ended(history_db, "generated_ended_scene")
+    except Exception:
+        assert 0
+    else:
+        assert 1
+
 
 
 def test_change_scene(setup, create_one_scene, scene_root, history_db):
-    scene = dummy_object.make_dummy_scene("scene")
+    from src.mtool.scene import current_scene
 
-    change_scene = entry_scene.change_scene(scene, scene_root, history_db)
-    assert change_scene == "scene"
+    scene = dummy_object.make_dummy_scene("scene")
+    
+    entry_scene.change_scene(scene, scene_root, history_db)
+    current_scene = current_scene.current_scene(scene_root, history_db)
+
+    assert current_scene == "scene"
 
 
 def test_end_scene(setup, create_one_scene, scene_root, history_db, current_scene_db):
-    ended = entry_scene.end_scene("generated_one_scene", scene_root, history_db, current_scene_db)
+    from src.mtool.scene import list_scene
+    from src.mtool.util.sqlite import sqlite_scene
+    from src.mtool.util import error
 
-    assert ended == "generated_one_scene"
+    entry_scene.end_scene("generated_one_scene", scene_root, history_db, current_scene_db)
+
+    try:
+        sqlite_scene.check_scene_ended(history_db, "generated_one_scene")
+    except error.SceneEndedError:
+        assert 1
+    else:
+        assert 0
 
 
 def test_new_scene(setup, scene_root, history_db):
     from src.mtool.scene import delete_scene
+    from src.mtool.scene import current_scene
 
     scene = dummy_object.make_dummy_scene("generated_new_scene")
 
-    new_scene = entry_scene.new_scene(scene, scene_root, history_db)
-    assert new_scene[1] == "generated_new_scene"
+    entry_scene.new_scene(scene, scene_root, history_db)
+    current_scene = current_scene.current_scene(scene_root, history_db)
+
+    assert current_scene == "generated_new_scene"
 
     delete_scene.delete_scene("generated_new_scene", scene_root, history_db)
 
 
 def test_history(setup, generate_short_history, scene_root, history_db, library_db, current_scene_db):
-    assert len(entry_scene.history("", scene_root, history_db, library_db, current_scene_db)) == 1
+    from src.mtool.scene import history
+    entry_scene.history("", scene_root, history_db, library_db, current_scene_db)
+    history = history.history(history_db, library_db, current_scene_db)
+
+    assert len(history) == 1
 
