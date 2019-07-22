bfe4917e47a7f314acf8181f4c604cf98d6bc66b
Amanda Bertsch <t-ambert@microsoft.com>
2019-07-22 16:25:28 -0700

    fixes spookiness caused by being an absolute fool

--- src/training/mtool-training-app/mtool_model.py
+++ src/training/mtool-training-app/mtool_model.py
@@ -1,4 +1,25 @@
-# build a LSTM model with mtool notebook execution sequences for predicting next
+"""
+build a LSTM model with mtool notebook execution sequences for predicting next
+
+questions:
+SHAPE: what's going on? what should shapes be? how do I shape from generator? do I need to flatten?
+    --> shape is now fixed! reshape to 3D array, batch as first dimension, timestep, then feature
+    --> flattening is unnecessary though output is 1 by <# of filenames> array
+
+FEATURES: am I right that features should be notebook sequence, etc? how do I add more?
+    --> yes
+    --> https://monkeylearn.com/blog/beginners-guide-text-vectorization/
+
+OHE: is this the move? what about non-sequential data like cell output? what about new notebooks being run? during retraining, do I remake the OHE?
+    --> link above, change series shaping at bottom 
+    --> will probably need to update converter every training cycle
+
+LAYERS: at what point (if any) should I look at adding more layers? what value do they add?
+    --> still not sure
+
+BATCH: is adding batches important? should batches be shuffled each time? how to choose batch size?
+    --> appears to be a hyperparameter?
+"""
 import os
 import time
 import numpy as np
@@ -14,41 +35,59 @@ from keras.models import load_model
 from keras.layers import LSTM
 from keras.layers import Dense
 
-from numpy import array
+from mtool_model_data import sequences
 from mtool_model_utils import *
 
-sequences = []
 ##
-# set the number of input steps to be used to predict the next
-# NOTE: currently: sets the number of input steps as the average of input sequence length
-# TODO: might want to choose value as hyperparameter? or better function
-# NOTE: why does the number of steps not affect complexity? 
-# NOTE: check out next code section. still not sure why training time impacted but not complexity
+# hyperparameters to tune
 ##
+# n_steps: the number of input steps to be used to predict the next notebook, avg of input seq length
 lens = pd.Series([len(s) for s in sequences])
-# lens.hist()                # NOTE: creates histogram of sequence lengths (?)
 desc = lens.describe()
-n_steps = int(desc['mean'])  # affects the training time not the complexity of the model
+n_steps = int(desc['mean'])   # affects the training time not the complexity of the model
+
+# n_units: LSTM units/cells,which determines the complexity of the model 
+# (sometimes called  size_of_output, n_neurons)
+n_units = n_steps  
+
+# split: fraction of data used to train (vs. used for validation)
+split = 2/3
+
+# batch: size of each batch (right now, 2 batches per epoch)
+batch = int((1/3) * len(sequences))
+
+# optimizer: using adam, as is standard
+# adam's base learning rate: 0.001
+from keras import optimizers
+adam = optimizers.adam(lr=0.001)
+
+# loss: also consider binary cross_entropy
+loss_func = 'categorical_crossentropy'
 
 ##
-# set the number of LSTM units/cells
-##
-n_units = n_steps  # i.e., size_of_output, which determines the complexity of the model
-
-##
-# get the name of all notebooks to be used as features
+# data summary: view frequencies of notebooks in dataset
 ##
 notebooks = sum(sequences, [])
 frequency = pd.Series(notebooks).value_counts()
 print(frequency)
-features = sorted(set(notebooks))
-n_features = len(features)  # i.e. size_of_input
 
-##
-# train an OHE with all features
-##
-ohe = OneHotEncoder(handle_unknown='ignore', sparse=False)
-ohe.fit(array(features).reshape(n_features, 1))
+# features considered (right now, just filenames)
+n_features = 1
+
+# first approximation, assigns each sequence a numerical equiv 
+# TODO: use Tokenizer? OHE? cleaner solution?
+seqs, converter = basic_counter(sequences)
+#ohe = OneHotEncoder(converter, handle_unknown='ignore', sparse=False)
+
+# unique_file_count: size of OHE (could make larger to allow for expansion (?))
+unique_file_count = len(converter) 
+
+seed = 42
+np.random.seed(seed)  # make the run deterministic
+
+np.random.shuffle(seqs)
+train_seq = seqs[:int(len(seqs)*split)]
+valid_seq = seqs[int(len(seqs)*split):]
 
 
 ##
@@ -56,31 +95,31 @@ ohe.fit(array(features).reshape(n_features, 1))
 ##
 def define_compile_model():
     model = Sequential()
+    
+    # input_shape = (time_step, seq_len)
+    #    time_step = # timesteps considered
+    #    seq_len= # of features considerd 
     model.add(LSTM(n_units, input_shape=(n_steps, n_features)))
-    model.add(Dense(n_features, activation='softmax'))
 
-    # https://stackoverflow.com/questions/38080035/how-to-calculate-the-number-of-parameters-of-an-lstm-network
-    # LSTM params = 4 * ((size_of_input + 1) * size_of_output + size_of_output^2)
-    #             = 4 * size_of_output * (size_of_input + 1 + size_of_output)
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+    # size = length of OHE array
+    model.add(Dense(unique_file_count, activation='softmax'))
+    
+    model.compile(loss=loss_func, optimizer=adam, metrics=['acc'])
     print(model.summary())
-
     return model
+
+define_compile_model()
 
 
 ##
 # fit the model
 ##
-xs, ys = generate_all_samples(sequences, n_steps, ohe)
-print('xs.shape: ' + str(xs.shape), '; ys.shape: ' + str(ys.shape))
-split = 1/3
-
-seed = 42
-np.random.seed(seed)  # make the run deterministic
+training_gen = generate(seqs, batch_size=batch, num_steps=n_steps, total_names=unique_file_count)
+validation_gen = generate(seqs, batch_size=batch, num_steps=n_steps, total_names=unique_file_count)
 
 start = time.time()
 model = define_compile_model()
-history = model.fit(xs, ys, batch_size=1, epochs=100, verbose=2, validation_split=split, shuffle=False)
+history = model.fit_generator(training_gen, len(train_seq)//(batch), epochs=100, validation_data=validation_gen, validation_steps = len(valid_seq)//(batch*n_steps) +1, verbose=2)
 end = time.time()
 elapsed = end - start
 
@@ -100,61 +139,47 @@ epoch_min_val = np.argmin(history.history['val_loss'])
 ##
 # evaluate the model with train/test split
 ##
-x_train, x_test, y_train, y_test = train_test_split(xs, ys, test_size=split, random_state=seed)
-
-seed = 42
-np.random.seed(seed)
+training_gen = generate(seqs, batch_size=batch, num_steps=n_steps, total_names=unique_file_count)
+validation_gen = generate(seqs, batch_size=batch, num_steps=n_steps, total_names=unique_file_count)
 
 start = time.time()
 model = define_compile_model()
-history = model.fit(x_train, y_train, batch_size=1, epochs=epoch_min_val, verbose=2, shuffle=False)
+history = model.fit_generator(training_gen, len(train_seq)//(batch), epochs=epoch_min_val, verbose=2)
 end = time.time()
 elapsed = end - start
 
-model.evaluate(x_test, y_test)
+##
+# export the lstm model to a single file
+##
+# TODO: currently dumps model in same directory, let's... not
+model_file = 'lstm_model.h5'
+model_path = os.path.join(os.path.dirname(__file__), "models", model_file) 
+model.save(model_path)
 
 ##
 # make predictions with the model
 ##
-seq_new = sequences[-1]
-samples = generate_samples(seq_new, n_steps, ohe)
+np.random.shuffle(seqs)
+num_tests = 3
 
-for i, (x, y) in enumerate(samples):
-    yhat_proba = model.predict_proba(x)  # model.predict(x)
-    print('\nSequence: %d' % i)
-    print('Sequence: %s' % [one_hot_decode(x, ohe) for x in x])
-    print('Expected: %s' % one_hot_decode(y, ohe))
-    print('Predicted: %s' % one_hot_decode(yhat_proba, ohe))
-    print('\n%s' % str(pd.Series(yhat_proba[0], ohe.categories_).sort_values(ascending=False)))
+sample = generate(seqs[:num_tests], batch_size=1, num_steps=n_steps, total_names=unique_file_count)
 
-    # yhat_class = model.predict_classes(x)
-    # print('Predicted: %s' % ohe.categories_[yhat_class[0]])
+for i in range(num_tests):
+    x, y = next(sample)
+    yhat_proba = model.predict(x)
+    print('\nSequence: %d' % (i+1))
+    print('Sequence: %s' % [decode(val, converter) for val in x[0]])
+    print('Expected: %s' % one_hot_decode(y, converter))
+    print('Predicted:')
 
-##
-# export the lstm model to a single file and then import
-##
-app_dir = "mtool-model-app"
-model_file = 'lstm_model.h5'
-model_path = os.path.join(app_dir, model_file)
-model.save(model_path)
+    print(pd.Series(yhat_proba[0], converter).sort_values(ascending=False))
 
-model_loaded = load_model(model_path)
-model_loaded.evaluate(x_test, y_test)
+#model.evaluate(validation_gen)
 
-seq_new = sequences[-1]
-x = generate_features(seq_new, n_steps, ohe)
-yhat_proba = model.predict(x)
-series = pd.Series(yhat_proba[0], index=pd.Index(ohe.categories_[0], name='notebook'), name='probability')
-series_sorted = series.sort_values(ascending=False)
-print('\n%s' % str(series_sorted.reset_index()))
 
 ##
-# export the ohe and then import
+# export the converter as a pickle
 ##
-
-ohe_file = 'onehot_encoder.pkl'
-ohe_path = os.path.join(app_dir, ohe_file)
-joblib.dump(ohe, ohe_path, compress=9)
-ohe_loaded = joblib.load(ohe_path)
-encoded = ohe_loaded.transform(array(features).reshape(n_features, 1))
-print(encoded)
+converter_file = 'convert.pkl'
+converter_path = os.path.join(os.path.dirname(__file__), converter_file)
+joblib.dump(converter, converter_path, compress=9)
--- src/training/mtool-training-app/mtool_model_utils.py
+++ src/training/mtool-training-app/mtool_model_utils.py
@@ -1,58 +1,77 @@
-import re
-import subprocess
-import json
 
-MTOOL_ROOT= "http://nmnode-0-svc:50070/webhdfs/v1/mtool/"
+import collections 
 
-def test(loc):
+import numpy as np
+from numpy import array
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+
+def basic_counter(sequences):
+    converter = ["<None>", "<Unknown>"]
+    newSeqs = []
+    for seq in sequences:
+        newSeq = []
+        for filename in seq:
+            if filename not in converter:
+                converter.append(filename)
+            newSeq.append(converter.index(filename))
+        newSeqs.append(newSeq)
+    return newSeqs, converter
+
+def pad_sequence(sequence, length, value=0):
+    if len(sequence) <= length:
+        padded = pad_sequences([sequence], maxlen=length, dtype=object, value=value)
+        return padded[0].tolist()
+    else:
+        return sequence[(len(sequence) - length):]
+
+
+def one_hot_encode(sequence, ohe):
+    encoded = ohe.transform(array(sequence).reshape(len(sequence), 1))
+    return encoded
+
+def generate(sequences, batch_size, num_steps, total_names):
+    x = np.zeros((batch_size, num_steps))
+    #y = np.zeros((batch_size, num_steps, total_names))
+    y= np.zeros((batch_size, total_names))
+    i = 0
+    while True:
+        for sequence in sequences:
+            for j in range(1, len(sequence)):
+                x[i, :] = pad_sequence(sequence[:j], num_steps)
+                # OHE based on sequence
+                temp = [0] * total_names
+                temp[sequence[j]] = 1
+                y[i, :] = temp #to_categorical(sequence[j],num_classes=total_names) #to_categorical(pad_sequence(sequence[:j+1], num_steps), num_classes=total_names)
+                i += 1
+                if i == batch_size:
+                    yield x.reshape(batch_size,num_steps,1), y
+                    i = 0
+
+def generate_features(sequence, length, ohe):
+    padded = pad_sequence(sequence, length)
+    encoded = one_hot_encode(padded, ohe)  # TODO: possible to avoid ohe with tensorflow?
+
+    features = ohe.categories_[0]
+    n_features = len(features)
+
+    # reshape sequence to be 3D
+    x = encoded[-length:].reshape((1, length, n_features))
+
+    return x
+
+def decode(encoded, converter):
+    decoded = converter[int(encoded[0])]
+    return decoded
+
+def one_hot_decode(encoded, converter):
+    index = encoded.tolist()[0].index(1)
+    return converter[int(index)]
     """
-    collect the sequences for all users and all scenes.
-    should we record user/scene/time info for training?
+    decoded = ohe.inverse_transform(encoded)
+    return decoded.squeeze().tolist()
     """
-    import requests
-    from datetime import date, timedelta
 
-    date_path = (date.today() - timedelta(days=int(loc))).strftime('%Y/%m/%d/ipynb')
-
-    global MTOOL_ROOT   
-    basepath = MTOOL_ROOT + date_path
-
-    params = (
-        ('op', 'LISTSTATUS'),
-    )
-
-    response = requests.get(basepath, params=params)
-    
-    users = (response.json())['FileStatuses']['FileStatus']
-
-    sequences = []
-    for user in users:
-        user_basepath = basepath + "/" + user['pathSuffix']
-        response = requests.get(user_basepath, params=params)
-    
-        scenes = (response.json())['FileStatuses']['FileStatus']
-        for scene in scenes:
-            scene_basepath = user_basepath + "/" + scene['pathSuffix']
-            response = requests.get(scene_basepath, params=params)
-            files = (response.json())['FileStatuses']['FileStatus']
-            sequence = []
-            for f in files:
-                fullname = f['pathSuffix']
-                fullname.rstrip('.ipynb')
-                # removes datetime info at start of string, and library name
-                filename = ('-'.join(fullname.split('-')[3:])) #.rstrip('.ipynb')
-                filename = filename[0:len(filename)-6] # removes .ipynb
-                sequence.append(filename)
-            sequences.append(sequence)
-
-    return sequences
-
-    """http://nmnode-0-svc:50070/webhdfs/v1/mtool?op=LISTSTATUS"""
-
-    """
-    1. find model file
-    2. load_model() on filename
-    3. fit_on_batch or train_on_batch of sequences? OHE first tho 
-       - make sure we're training on full names, not just prefixes!
-    4. save model to current date!
-    """
+def highest_prob_five(results, converter): 
+    sorted = pd.Series(yhat_proba, converter).sort_values(ascending=False)
+    print(sorted)
