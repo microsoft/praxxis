2459607b9055668080665cd289cbd3276357c528
Skyler Jane Clark <t-skclar@microsoft.com>
2019-07-29 16:36:26 -0700

    removes 3.7 because it's confusing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
      name='praxxis', 
-     python_requires = '>=3.7.*, >=3.6.*',
+     python_requires = '>=3.6.*',
      install_requires=[
         "urllib3",
         "requests",
