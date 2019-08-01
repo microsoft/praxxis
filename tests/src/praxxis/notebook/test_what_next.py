8f42e9ed3b8ffc66d76bd26b0fd32383bcdb539a
Amanda Bertsch <t-ambert@microsoft.com>
2019-08-01 15:18:44 -0700

    fixes broken test

--- tests/src/praxxis/notebook/test_what_next.py
+++ tests/src/praxxis/notebook/test_what_next.py
@@ -1,11 +1,11 @@
 import pytest
 
 def test_what_next_rulesengine(setup, telemetry_db, current_scene_db, 
-        library_db, rulesengine_db, start, stop, generate_short_history,
+        library_db, rulesengine_db, model_db, start, stop, generate_short_history,
         create_one_ruleset_one_rule):
     from src.praxxis.notebook import what_next
 
-    predictions = what_next.what_next("", telemetry_db, current_scene_db, library_db, rulesengine_db, start, stop)
+    predictions = what_next.what_next("", telemetry_db, current_scene_db, library_db, rulesengine_db, model_db, start, stop)
 
     assert len(predictions) == 2
     assert predictions[0][0] == "pred1"
