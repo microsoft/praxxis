08c5a51e45e8d9315e3413e02e35c324ebef82df
Skyler Jane Clark <skylerjaneclark@gmail.com>
2019-10-01 19:31:53 -0400

    adds a minor version for pypi update

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         "colorama",
         "toml"
      ],
-     version='0.1.0',
+     version='0.1.1',
      author="Skyler Clark, Amanda Bertsch, Swarathmika Kakivaya, Stuart Padley",
      author_email="skylerjaneclark@gmail.com, abertsch72@gmail.com, swarathmika@hotmail.com",
      description="a notebook task interface built on big data and machine learning.",
